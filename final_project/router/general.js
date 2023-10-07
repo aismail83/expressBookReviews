2c91d8a950237437c912e57dbebf84ac89d3ae8c
aismail83 <ismailaltoumalsadick@gmail>
2023-10-07 06:59:33 -0400

    First release of Hello World!

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,22 +28,31 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   let author  = req.params.author;
-  const bookAuthors = books.filter((book)=>
+  const bookAuthors = Object.values(books).filter((book)=>
      book["author"] === author
   );
-  
+
   res.send(JSON.stringify(bookAuthors,null,4));});
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title  = req.params.title;
+  const bookTitles = Object.values(books).filter((book)=>
+     book["title"] === title
+  );
+  res.send(JSON.stringify(bookTitles,null,4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn  = req.params.isbn;
+  const bookReviews = Object.values(books).filter((book)=>
+    book === book["isbn"]).map((book)=>
+     book === book["reviews"]
+  );
+  res.send(JSON.stringify(bookReviews,null,4));
 });
 
 module.exports.general = public_users;
